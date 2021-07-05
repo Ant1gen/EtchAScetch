c1f97eba39b0674cecb429b1f872ec59414077b3
Ant1gen <antigen88@gmail.com>
2021-07-05 13:38:27 +0300

    added functions to clear grid, added button with function to reset grid, and event to turn grey divs to black on mouseOver

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 let container = document.getElementById("container");
-//container.innerHTML = "<p>Test</p>";
+let button = document.getElementById("reset");
+
 function addDiv() {
     let div = document.createElement('div');
     div.classList.add('etch');
-    //div.setAttribute('style', 'border : 3px solid white; background : grey; width : 40px; height : 40px');
-    
     container.appendChild(div);
+    div.addEventListener('mouseover', () => {div.setAttribute('style', 'background : black')});
 }
-for (let i = 0; i< 256; i++){
-    addDiv();
+function fillGrid(sideSize){
+    container.setAttribute('style', `grid-template-columns: repeat(${sideSize}, auto); grid-template-rows: repeat(${sideSize}, auto);`)
+let squaresInGrid = sideSize*sideSize;
+    for (let i = 0; i< squaresInGrid; i++){
+        addDiv();
+    }
 }
+
+function clearGrid() {
+    container.innerHTML = '';
+}
+
+function reset(){
+    clearGrid();
+    let size = prompt('Size.  maximum 100', '16');
+    size = Number(size);
+    if (size < 0){
+        return;
+    }
+    else if (size > 100){
+        fillGrid(100);
+    }
+    else {
+    fillGrid(size);
+    }
+}
+button.addEventListener('click', reset);
